f1bf3858a15105d36c084fc994043c3bd19d2c73
Sergey Pavlov <sergey.pavlov@datium.fi>
2020-05-05 20:57:32 +0300

    supported objects check

--- CoreProbe1/wwwroot/js/utils/util-checker.js
+++ CoreProbe1/wwwroot/js/utils/util-checker.js
@@ -20,13 +20,40 @@ var app;
                     }
                     return results;
                 }
-                areSupported(propertyNames) {
+                checkObjects(objectNames) {
+                    const results = [];
+                    for (let i = 0; i < objectNames.length; i++) {
+                        const objName = objectNames[i];
+                        const res = {
+                            propertyName: objName,
+                            isSupported: !(typeof window[objName] === 'undefined')
+                        };
+                        results.push(res);
+                    }
+                    return results;
+                }
+                areSupportedProperties(propertyNames) {
                     const results = this.checkProperties(propertyNames);
                     const notSupported = results.filter(item => !item.isSupported);
                     return notSupported.length == 0;
                 }
-                showNotSupported(propertyNames, template, destination) {
-                    if (!this.areSupported(propertyNames)) {
+                areSupportedObjects(objectNames) {
+                    const results = this.checkObjects(objectNames);
+                    const notSupported = results.filter(item => !item.isSupported);
+                    return notSupported.length == 0;
+                }
+                showNotSupportedProperties(propertyNames, template, destination) {
+                    if (!this.areSupportedProperties(propertyNames)) {
+                        const content = app.util.dom.getTemplate(template);
+                        const dest = document.querySelector(destination);
+                        dest.innerHTML = "";
+                        dest.appendChild(content);
+                        return false;
+                    }
+                    return true;
+                }
+                showNotSupportedObjects(objectNames, template, destination) {
+                    if (!this.areSupportedObjects(objectNames)) {
                         const content = app.util.dom.getTemplate(template);
                         const dest = document.querySelector(destination);
                         dest.innerHTML = "";
--- CoreProbe1/wwwroot/js/utils/util-checker.ts
+++ CoreProbe1/wwwroot/js/utils/util-checker.ts
@@ -27,15 +27,49 @@ namespace app.util.check {
             return results;
         }
 
-        public areSupported(propertyNames: string[]): boolean {
+        public checkObjects(objectNames: string[]): ICheckResult[] {
+            const results: ICheckResult[] = [];
+
+            for (let i = 0; i < objectNames.length; i++) {
+                const objName = objectNames[i];
+                const res: ICheckResult = {
+                    propertyName: objName,
+                    isSupported: !(typeof window[objName] === 'undefined')
+                }
+                results.push(res);
+            }
+
+            return results;
+        }
+
+        public areSupportedProperties(propertyNames: string[]): boolean {
             const results = this.checkProperties(propertyNames);
             const notSupported = results.filter(item => !item.isSupported);
             return notSupported.length == 0;
         }
 
-        public showNotSupported(propertyNames: string[], template: string, destination: string): boolean {
+        public areSupportedObjects(objectNames: string[]): boolean {
+            const results = this.checkObjects(objectNames);
+            const notSupported = results.filter(item => !item.isSupported);
+            return notSupported.length == 0;
+        }
 
-            if (!this.areSupported(propertyNames)) {
+        public showNotSupportedProperties(propertyNames: string[], template: string, destination: string): boolean {
+
+            if (!this.areSupportedProperties(propertyNames)) {
+                const content = app.util.dom.getTemplate(template);
+                const dest = document.querySelector(destination);
+                dest.innerHTML = "";
+                dest.appendChild(content);
+                return false;
+            }
+
+            return true;
+        }
+
+        public showNotSupportedObjects(objectNames: string[], template: string, destination: string): boolean {
+
+            if (!this.areSupportedObjects(objectNames)) {
                 const content = app.util.dom.getTemplate(template);
                 const dest = document.querySelector(destination);
                 dest.innerHTML = "";
