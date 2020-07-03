a2d009af7a3d984011f39e7f058ea108d4ab82af
Sergey Pavlov <sergey.pavlov@datium.fi>
2020-07-03 17:06:55 +0300

    adding zenit

--- MedaProbe/Controllers/EffectController.cs
+++ MedaProbe/Controllers/EffectController.cs
@@ -22,5 +22,10 @@ namespace CoreProbe1.Controllers
         {
             return View();
         }
+
+        public IActionResult VideoZenit()
+        {
+            return View();
+        }
     }
 }
